da2cc771a1bb92c298b36749dbf22bf52aacfa57
heon-kim <vndtka123@gmail.com>
2024-09-11 17:11:08 +0900

    ♻️ refactor ex2.js

--- 김해원/ex2.js
+++ 김해원/ex2.js
@@ -18,14 +18,11 @@ const range = (start, end, step = start > end ? -1 : 1) => {
     return [];
   }
 
-  if (step < 0) {
-    for (let i = start; i >= end; i = Number((i + step).toFixed(1))) {
-      resultArry.push(i);
-    }
-  } else {
-    for (let i = start; i <= end; i = Number((i + step).toFixed(1))) {
-      resultArry.push(i);
-    }
+  const condition = step < 0 ? (i) => i >= end : (i) => i <= end;
+  const operation = (i) => Math.round((i + step) * 10) / 10;
+
+  for (let i = start; condition(i); i = operation(i)) {
+    resultArry.push(i);
   }
 
   return resultArry;
