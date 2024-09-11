3abb47f8cd687aed754931bef46dc4a4ac543876
heon-kim <vndtka123@gmail.com>
2024-09-11 22:13:03 +0900

    ✨ Solve ex4

--- 김해원/ex4.js
+++ 김해원/ex4.js
@@ -1,3 +1,9 @@
-function deepCopy(obj) {}
+function deepCopy(obj) {
+  return {
+    ...obj,
+    arr: JSON.parse(JSON.stringify(obj.arr)),
+    oo: JSON.parse(JSON.stringify(obj.oo)),
+  };
+}
 
 module.exports = { deepCopy };
