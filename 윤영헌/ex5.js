868ff418f1696ea2a0592dae946e838fab7e40d8
yoounyoungheon <107925716+yoounyoungheon@users.noreply.github.com>
2024-09-11 18:33:02 +0900

    solve 4

--- 윤영헌/ex5.js
+++ 윤영헌/ex5.js
@@ -1,3 +1,7 @@
 module.exports = {
-  searchByKoreanInitialSound: (data, firstSounds) => {},
+  searchByKoreanInitialSound: (data, firstSounds) => {
+    
+  },
 };
+
+s = ['강원도 고성군', '고성군 토성면', '토성면 북면', '북면', '김1수'];
