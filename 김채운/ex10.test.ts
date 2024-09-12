f89a96c18cc32aee50f82afe1a7426100bc7fa46
codnscodns <dnstm1119@naver.com>
2024-09-12 22:13:46 +0900

    create ex7

--- 김채운/ex10.test.ts
+++ 김채운/ex10.test.ts
@@ -1,4 +1,18 @@
 import { ArrayList } from './ex10';
 console.log('🚀  ArrayList:', ArrayList);
 
-// 여기에 테스트코드를 작성하세요.
+const alist = new ArrayList([1, 2]); // alist.toString() ⇒ { value: 1, rest: { value: 2 } }
+alist.add(3); // { value: 1, rest: { value: 2, rest: { value: 3 } } }
+alist.add(5, 1); // { value: 1, rest: { value: 5, rest: { value: 2, rest: { value: 3 } }}
+// alist.remove(2); // { value: 1, rest: { value: 5, rest: { value: 3 } } }
+alist.add(22, 1); // { value: 1, rest: { value: 22, rest: { value: 5, rest: { value: 3 } } } }
+alist.add(33, 1);
+alist.toString(); // ArrayList(4) { value: 1, rest: { value: 33, rest: { value: 22, rest: { value: 5, rest: ...} } } }
+alist.set(1, 300); // { value: 1, rest: { value: 300, rest: { value: 33, rest: { ... } } } }
+alist.get(2); alist.size; // 22, 4
+alist.indexOf(300); // 1
+alist.contains(300); alist.contains(301); // true, false
+alist.isEmpty; alist.peek; // false, 3
+alist.toArray(); // [1, 300, 22, 3]
+alist.iterator().next(); // { value: 1, done: false }
+alist.clear(); // all clear
