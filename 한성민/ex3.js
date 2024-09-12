d500e163f9b2ed818f0267c5bcc50a06cfcfff51
kkx7787 <h016@alpacoedu.onmicrosoft.com>
2024-09-13 00:53:50 +0900

    Solve ex2, ex3, ex4

--- 한성민/ex3.js
+++ 한성민/ex3.js
@@ -1,3 +1,21 @@
 Array.prototype.sortBy = function (sortProp = '') {
-  return this;
+  const props = sortProp.split(','); // 정렬 요소 split
+
+  return this.slice().sort((a, b) => {
+    for (const prop of props) {
+      const [sortKey, order] = prop.split(':');
+      const direction = order === 'desc' ? -1 : 1;
+
+      if (a[sortKey] > b[sortKey]) return direction;
+      if (a[sortKey] < b[sortKey]) return -direction;
+    }
+    return 0; // 모든 정렬 기준이 동일하면 0 반환
+  });
 };
+
+const hong = { id: 1, name: 'Hong', city: 'Busan', dept: 1 };
+const kim = { id: 2, name: 'Kim', city: 'Seoul', dept: 2 };
+const lee = { id: 3, name: 'Lee', city: 'Daegu', dept: 2 };
+const users = [lee, hong, kim];
+console.log(users.sortBy('name:desc,id:,dept:desc'));
+
