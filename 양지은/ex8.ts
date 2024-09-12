25a3fa3aaaec27a45b44ed5953adda1099df2ff0
Ji-eun Yang <jidak1165@gmail.com>
2024-09-12 23:54:27 +0900

    [done] #8 완료

--- 양지은/ex8.ts
+++ 양지은/ex8.ts
@@ -1,27 +1,37 @@
-// function debounce(func, delay) {
-//     let timer;
-//     return function(...args) {
-//         clearTimeout(timer);
-//         timer = setTimeout(() => func.apply(this, args), delay);
-//     };
-// }
 
-// const searchBox = document.getElementById('searchBox');
+// 디바운스
+const debounce_ = (cb: (a: number) => void, delay: number) => {
+    let timer: ReturnType<typeof setTimeout> | null = null;
 
-// searchBox.oninput = debounce(() => {
-//     const query = searchBox.value;
-//     console.log(new Date().toISOString(), 'search>>', query);
-// }, 500);
+    return (i: number) => {
+        if (timer) {
+            clearTimeout(timer);
+        }
 
-const debounce = (cb: any, delay: number) => (i: number) => {
-
+        timer = setTimeout(() => {
+            cb(i);
+        }, delay);
+    };
 };
-const throttle = (cb: any, delay: number) => (i: number) => {};
+
+// 쓰로틀
+const throttle_ = (cb: (a: number) => void, delay: number) => {
+    let timer: ReturnType<typeof setTimeout> | null = null;
+
+    return (i: number) => {
+        if (!timer) {
+            cb(i);
+            timer = setTimeout(() => {
+                timer = null;
+            }, delay);
+        }
+    };
+}
 
 // function throttle...
 
-const debo = debounce((a: number) => console.log(a + 1), 500);
-for (let i = 10; i < 15; i++) debo(i); // 15 출력
+const debo_ = debounce_((a: number) => console.log(a + 1), 500);
+for (let i = 10; i < 15; i++) debo_(i); // 15 출력
 
-const thro = throttle((a: number) => console.log(a + 1), 500);
-for (let i = 10; i < 15; i++) thro(i); // 11 출력
+const thro_ = throttle_((a: number) => console.log(a + 1), 500);
+for (let i = 10; i < 15; i++) thro_(i); // 11 출력
