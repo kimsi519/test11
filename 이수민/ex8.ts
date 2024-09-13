a1471650b253f85b9c00d8d75dedb638867e1b27
suminjeff <suminjeff@gmail.com>
2024-09-13 10:39:10 +0900

    ex8 (feat) completed

--- 이수민/ex8.ts
+++ 이수민/ex8.ts
@@ -1,11 +1,39 @@
 // dummy(mock)입니다. 올바르게 수정하세요.
-const debounce = (cb: any, delay: number) => (i: number) => {};
-const throttle = (cb: any, delay: number) => (i: number) => {};
+const debounce_suminlee = <T extends any[]>(
+  cb: (...args: T) => void,
+  delay: number
+) => {
+  let timer: NodeJS.Timeout | null = null;
+  // debounce 되는 함수
+  return (...args: any) => {
+    // 기존 타이머가 있다면 제거
+    if (timer) clearTimeout(timer);
+    // 새 타이머 설정
+    timer = setTimeout(() => {
+      cb(...args);
+    }, delay);
+  };
+};
+
+const throttle_suminlee = <T extends any[]>(
+  cb: (...args: T) => void,
+  delay: number
+) => {
+  let lastCalled = 0;
+  // throttle 되는 함수
+  return (...args: T) => {
+    const now = Date.now(); // 현재 시각을 기준으로 마지막 실행 시간이 delay보다 크다면 cb 호출
+    if (now - lastCalled >= delay) {
+      cb(...args);
+      lastCalled = now; // 마지막 호출 업데이트
+    }
+  };
+};
 
 // function throttle...
 
-const debo = debounce((a: number) => console.log(a + 1), 500);
-for (let i = 10; i < 15; i++) debo(i); // 15 출력
+const debo_suminlee = debounce_suminlee((a: number) => console.log(a + 1), 500);
+for (let i = 10; i < 15; i++) debo_suminlee(i); // 15 출력
 
-const thro = throttle((a: number) => console.log(a + 1), 500);
-for (let i = 10; i < 15; i++) thro(i); // 11 출력
+const thro_suminlee = throttle_suminlee((a: number) => console.log(a + 1), 500);
+for (let i = 10; i < 15; i++) thro_suminlee(i); // 11 출력
