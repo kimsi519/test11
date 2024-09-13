f0b7dabf7ea5f2b04894385da0ae309d6e1c5950
suzinlim <122861956+suzinlim@users.noreply.github.com>
2024-09-13 15:45:16 +0900

    submit assignment

--- 임수진/ex3.js
+++ 임수진/ex3.js
@@ -1,3 +1,17 @@
 Array.prototype.sortBy = function (sortProp = '') {
-  return this;
+    const sortArray = sortProp.split(',');
+
+    // 각 기준을 [sortKey, order] 형태로 변환
+    const sortPairs = sortArray.map(value => {
+        const [sortKey, order] = value.split(':');
+        return [sortKey, order || 'asc']; // order가 비어있을 경우 기본값 'asc'로 설정
+    });
+
+    return this.slice().sort((a, b) => {
+        for (const [sortKey, order] of sortPairs) {
+            if (a[sortKey] > b[sortKey]) return order === 'desc' ? -1 : 1;
+            if (a[sortKey] < b[sortKey]) return order === 'desc' ? 1 : -1;
+        }
+        return 0;
+    });
 };
--- 임수진/ex7.ts
+++ 임수진/ex7.ts
@@ -1,3 +1,63 @@
 const POST_URL = 'https://jsonplaceholder.typicode.com/posts';
 
-export async function getPosts(userId: number | string) {}
+interface Post {
+    userId: number;
+    id: number;
+    title: String;
+    body: String;
+}
+
+interface Comment {
+    postId: number;
+    id: number;
+    email: String;
+    body: String;
+}
+
+interface CommentResponse extends Comment {
+    name: String;
+}
+
+interface PostComment {
+    postId: number;
+    title: String;
+    comments: Comment[];
+}
+
+export async function getPosts(userId: number | string): Promise<PostComment[]> {
+    try {
+        // userId로 게시글 조회
+        const postResponse = await fetch(`${POST_URL}?userId=${userId}`);
+        const posts: Post[] = (await postResponse.json()) as Post[];
+
+        // 결과 반환할 객체 배열
+        const postComments: PostComment[] = await Promise.all(
+            posts.map(async (post: Post) => {
+                const commentResponse = await fetch(`${POST_URL}/${post.id}/comments`);
+                const comments: CommentResponse[] =
+                (await commentResponse.json()) as CommentResponse[];
+
+                // 결과 반환할 객체
+                let postComment: PostComment = {
+                postId: post.id,
+                title: post.title,
+                comments: [],
+                };
+
+                comments.forEach((comment: Comment) => {
+                    postComment.comments.push({
+                        postId: comment.postId,
+                        id: comment.id,
+                        email: comment.email,
+                        body: comment.body,
+                    });
+                });
+                return postComment;
+            })
+        );
+        return postComments;
+    } catch (error) {
+        console.error('에러 나요ㅠ.ㅠ:', error);
+        return [];
+    }
+}
