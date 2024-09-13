a402f2ce7fee05a21044597336a8967afabc3996
codnscodns <dnstm1119@naver.com>
2024-09-13 15:14:51 +0900

    update button hover

--- 김채운/ex2.js
+++ 김채운/ex2.js
@@ -8,21 +8,19 @@ const range = (start, end, step = start > end ? -1 : 1) => {
         start = start < 0 ? start : 1; // 음수일 경우 그대로 사용하고, 양수일 경우 1부터 시작
     }
 
-    // step이 0인 경우, [start]만 반환
     if (step === 0) {
         return [start];
     }
 
     const result = [];
 
-    // 소수점 처리를 위한 precision 설정
+    // 소수점 처리
     const precision = Math.max(
         (step.toString().split('.')[1] || '').length,
         (start.toString().split('.')[1] || '').length,
         (end.toString().split('.')[1] || '').length
     );
 
-    // 정밀도를 가진 step, start, end를 위한 수치
     const x = Math.pow(10, precision);
     start = Math.round(start * x);
     end = Math.round(end * x);
@@ -32,7 +30,6 @@ const range = (start, end, step = start > end ? -1 : 1) => {
     if (step > 0) {
         if (start <= end) {
             for (let i = start; i <= end; i += step) {
-                // 결과를 원하는 precision으로 반올림
                 result.push(parseFloat((i / x).toFixed(precision)));
             }
         }
@@ -41,7 +38,6 @@ const range = (start, end, step = start > end ? -1 : 1) => {
     else {
         if (start >= end) {
             for (let i = start; i >= end; i += step) {
-                // 결과를 원하는 precision으로 반올림
                 result.push(parseFloat((i / x).toFixed(precision)));
             }
         }
